5ccde22d4b10fffc42577b97d6327ce511650ab2
tipanyya06 <stephanieguce@gmail.com>
2024-10-24 17:00:54 +0800

    Layout Adjustment -bookspage fetct from database home page layout of pics (categories, books, authors)

--- src/steph/BooksPageComponents/ShowAllBooks.jsx
+++ src/steph/BooksPageComponents/ShowAllBooks.jsx
@@ -1,24 +1,43 @@
-import React from 'react'
-import { data, responsive } from '../data';
+
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 
 const ShowAllBooks = () => {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    const fetchBooks = () => {
+        axios
+        .get("http://localhost:8000/api/getBooks")
+        .then((response) => {
+            console.log(response.data);
+            setBooks(response.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    };
     return (
-        <div className='container'>
-            <div className='text-center font-bold text-3xl mt-5 mb-10 '>
+        <div className='container '>
+            <div className='text-center font-bold text-3xl mt-5 mb-[100px] '>
                 <h1>More Books</h1>
             </div>
-             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 p-6 ">
-                {data.map((book) => (
-                    <div 
-                    key={book.id}
-                    className= "bg-gray-100 shadow-xl rounded-xl overflow-hidden transition-transform transform hover:scale-105 hover:bg-red-950 hover:text-white max-w-[420px]">
-                    <img src={book.img} alt={book.name} className="w-full h-auto object-cover " />
-                    <div className="p-4">
-                        <h3 className="text-lg font-bold">{book.name}</h3>
-                        <p className="text-gray-500 mb-2">{book.category}</p>
-                        <p className="text-sm text-gray-700">{book.description}</p>
-                    </div>
+             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 ">
+                {books.map((book) => (
+                    <div className= " bg-slate-50 drop-shadow-xl rounded-xl sm:min-w-[250px]  transition-transform transform hover:scale-105 hover:bg-gray-100 group max-w-[420px] mb-[100px]">
+                        <div key={book.id} className='h-[150px] '> 
+                            <img src={book.bookcover} 
+                            alt={book.booktitle} 
+                            className="block mx-auto transform -translate-y-[100px]  h-60 object-contain" />
+                        </div>
+                        <div className="p-4 text-center ">
+                            <h3 className="text-lg font-bold ">{book.booktitle}</h3>
+                            <p className="text-sm text-gray-700">{book.bookdesc}</p>
+                        </div>
                     </div>
                 ))}
             </div>
