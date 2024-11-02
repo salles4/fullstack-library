d7a6e15731797b50b51a7c386f384e00bbcae0d1
tipanyya06 <stephanieguce@gmail.com>
2024-11-03 04:06:21 +0800

    Layout for AddAuthor/Book

--- src/josh/AddBook.jsx
+++ src/josh/AddBook.jsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
 import axios from 'axios';
+import bgImagee2 from "../steph/AboutPageComponents/bgg.png";
+
+const bgImage2 = {
+  backgroundImage: `url(${bgImagee2})`,
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+};
 
 const AddBook = () => {
-
   const [books, setBooks] = useState([]); 
   const [newBook, setNewBook] = useState({
     booktitle: '', 
@@ -67,110 +73,126 @@ const AddBook = () => {
   }
 
   return( 
-    <>
-    <h1 className='addbooktitle'>ADD BOOK PAGE</h1>
-
-    <div className='ctn'>
-
-    <div className='addbookctn1'>
-    <label>Book Title</label>
-    <input
-      className='inputbooktitle'
-      type="text"
-      placeholder="Book Title"
-      value={newBook.booktitle}
-      required
-      onChange={(e) => setNewBook({...newBook, booktitle: e.target.value})}
-    />
-    <label>Book Description</label>
-    <textarea
-      className='textareabookdesc'
-      type="text"
-      placeholder="Book Description"
-      value={newBook.bookdesc}
-      required
-      maxLength="300"
-      onChange={(e) => setNewBook({...newBook, bookdesc: e.target.value})}
-    />
-    <label>Book Cover</label>
-    <input
-      type="text"
-      placeholder="Book Cover"
-      value={newBook.bookcover}
-      required
-      onChange={(e) => setNewBook({...newBook, bookcover: e.target.value})}
-    />
-    </div>
-
-    <div className='addbookctn2'>
-    <label>Category</label>
-    <select value={newBook.category} onChange={(e) => setNewBook({...newBook, category: e.target.value})}>
-      <option value="" disabled>Select Category</option>
-      <option>General Knowledge</option>
-      <option>Philosophy and Psychology</option>
-      <option>Religion</option>
-      <option>Social Sciences</option>
-      <option>Languages</option>
-      <option>Science</option>
-      <option>Technology</option>
-      <option>Arts and Recreation</option>
-      <option>Literature</option>
-      <option>History and Geography</option>
-    </select>
-
-    <label>Author</label>
-    <input
-      type="text"
-      placeholder="Author"
-      value={newBook.author}
-      required
-      onChange={(e) => setNewBook({...newBook, author: e.target.value})}
-    />
-    <label>Publisher</label>
-    <input
-      type="text"
-      placeholder="Publisher"
-      value={newBook.publisher}
-      required
-      onChange={(e) => setNewBook({...newBook, publisher: e.target.value})}
-    />
-    <label>Shelf No.</label>
-    <input
-      type="number"
-      min="0"
-      placeholder="Shelf No."
-      value={newBook.shelfno}
-      required
-      onChange={(e) => setNewBook({...newBook, shelfno: e.target.value})}
-    />
-    <label>ISBN</label>
-    <input
-      type="number"
-      min="0"
-      maxLength={13}
-      placeholder="ISBN"
-      value={newBook.isbn}
-      required
-      onChange={(e) => setNewBook({...newBook, isbn: e.target.value})}
-    />
-    </div>
-    </div>
-    
-    <div className='btnctn'>
-    <button className="addbookbtn" onClick={handleCreateBook}>ADD BOOK</button>
-    <button className="resetbtn" onClick={clearEntries}>RESET</button>
-    <button className="cancelbtn">CANCEL</button>
-    </div>
-
-    {/* <h1 className='bookslisttitle'>BOOKS LIST</h1>
-      <ul>
-        {books.map(book => (
+    <main style={bgImage2} className="min-h-screen w-full flex items-center ">
+      <section className='container p-5 '>
+        <div className="container p-5 min-h-[750px] mx-auto" >
+          <h1 className="text-3xl font-bold mb-6 text-center text-white ">Add Book</h1>
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-5  p-6 rounded-lg shadow-xl shadow-gray-950 bg-white'>
+            <div>
+              <label className='block mb-2 '>Book Title</label>
+              <input
+                className='border rounded-lg w-full p-2'
+                type="text"
+                placeholder="Book Title"
+                value={newBook.booktitle}
+                required
+                onChange={(e) => setNewBook({...newBook, booktitle: e.target.value})}
+              />
+            </div>
+            <div>
+              <label className='block mb-2 '>Book Description</label>
+              <textarea
+                className='border rounded-lg w-full p-2'
+                placeholder="Book Description"
+                value={newBook.bookdesc}
+                required
+                maxLength="300"
+                onChange={(e) => setNewBook({...newBook, bookdesc: e.target.value})}
+              />
+            </div>
+            <div>
+              <label className='block mb-2 '>Book Cover</label>
+              <input
+                className='border rounded-lg w-full p-2'
+                type="text"
+                placeholder="Book Cover URL"
+                value={newBook.bookcover}
+                required
+                onChange={(e) => setNewBook({...newBook, bookcover: e.target.value})}
+              />
+            </div>
+            <div>
+              <label className='block mb-2 '>Category</label>
+              <select 
+                className='border rounded-lg w-full p-2'
+                value={newBook.category} 
+                onChange={(e) => setNewBook({...newBook, category: e.target.value})}>
+                <option value="" disabled>Select Category</option>
+                <option>General Knowledge</option>
+                <option>Philosophy and Psychology</option>
+                <option>Religion</option>
+                <option>Social Sciences</option>
+                <option>Languages</option>
+                <option>Science</option>
+                <option>Technology</option>
+                <option>Arts and Recreation</option>
+                <option>Literature</option>
+                <option>History and Geography</option>
+              </select>
+            </div>
+            <div>
+              <label className='block mb-2 '>Author</label>
+              <input
+                className='border rounded-lg w-full p-2'
+                type="text"
+                placeholder="Author"
+                value={newBook.author}
+                required
+                onChange={(e) => setNewBook({...newBook, author: e.target.value})}
+              />
+            </div>
+            <div>
+              <label className='block mb-2 '>Publisher</label>
+              <input
+                className='border rounded-lg w-full p-2'
+                type="text"
+                placeholder="Publisher"
+                value={newBook.publisher}
+                required
+                onChange={(e) => setNewBook({...newBook, publisher: e.target.value})}
+              />
+            </div>
+            <div>
+              <label className='block mb-2 '>Shelf No.</label>
+              <input
+                className='border rounded-lg w-full p-2'
+                type="number"
+                min="0"
+                placeholder="Shelf No."
+                value={newBook.shelfno}
+                required
+                onChange={(e) => setNewBook({...newBook, shelfno: e.target.value})}
+              />
+            </div>
+            <div>
+              <label className='block mb-2 '>ISBN</label>
+              <input
+                className='border rounded-lg w-full p-2'
+                type="text"
+                maxLength={13}
+                placeholder="ISBN"
+                value={newBook.isbn}
+                required
+                onChange={(e) => setNewBook({...newBook, isbn: e.target.value})}
+              />
+            </div>
+          </div>
           
-          <li key={book._id}>{book.booktitle} {book.bookdesc} <img className='bookcover' src={book.bookcover} alt='book cover'></img> {book.category} {book.author} {book.publisher} {book.shelfno} {book.isbn}</li>
-        ))}
-      </ul> */}
-    </>
-  )
+          <div className='flex justify-center items-center gap-2 mt-4'>
+            <button className="bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition duration-200" onClick={handleCreateBook}>
+                Submit
+            </button>
+            <button className="bg-gray-400 text-white py-2 px-4 rounded hover:bg-gray-500 transition duration-200" onClick={clearEntries}>
+                Reset
+            </button>
+            <button className="bg-red-600 text-white py-2 px-4 rounded hover:bg-red-700 transition duration-200" onClick={clearEntries}>
+                Cancel
+            </button>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default AddBook;
